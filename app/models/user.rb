2ad8e48e67d8e841deba021ee6b0df65c319c26e
Neto Concon <concon.neto2@gmail.com>
2019-12-18 17:12:11 -0300

    add all relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  enum kind: [:salesman, :manager]
+  enum status: [:active, :inactive]
+  has_many :comissions
+  has_many :addresses
+  has_many :clients
+  has_many :product_quantities
+  has_many :sales
 end
--- db/migrate/20191218194616_create_sales.rb
+++ db/migrate/20191218194616_create_sales.rb
@@ -3,7 +3,7 @@ class CreateSales < ActiveRecord::Migration[5.2]
     create_table :sales do |t|
       t.references :client, foreign_key: true
       t.date :sale_date
-      t.references :user, foreign_key: true
+      t.references :sale, foreign_key: true
       t.references :discount, foreign_key: true
       t.text :notes
 
