90cd105bd5c6e2696495554113b7cb359326d599
Neto Concon <concon.neto2@gmail.com>
2019-12-19 13:27:46 -0300

    fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,20 +3,28 @@ class Ability
 
   def initialize(user)
     if user
-      if user.kind == 'salesman'
-        can :access, :rails_admin
-        can :read, :dashboard
-        # can :dashboard
-        # can :manage, Client, user_id: user.id
-        # can :manage, Sale, user_id: user.id
-        can :read, Product, status: :active
-        can :read, Discount, status: :active
-        # can :read, Comission, user_id: user.id
-        # can :manage, ProductQuantity, user_id: user.id
-        # can :manage, Address, user_id: user.id
-      elsif user.kind == 'manager'
+      # if user.kind == 'salesman'
+      #   can :access, :rails_admin
+      #   can :read, :dashboard
+      #   # can :dashboard
+      #   can :manage, Client #, user_id: user.id
+      #   can :manage, Sale # , user_id: user.id
+      #   can :read, Product, status: :active
+      #   can :read, Discount, status: :active
+      #   can :read, Comission #, user_id: user.id
+      #   can :manage, ProductQuantity # , user_id: user.id
+      #   can :manage, Address #, user_id: user.id
+      # elsif user.kind == 'manager'
+      #   can :manage, :all
+      # end
+
+
+      if user.kind == "manager"
         can :manage, :all
       end
+
+
+
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,17 @@ User.destroy_all
 
 # Criando nossos Users --- OBS: Depois que adicionarmos o devise precisamos incluir o email e senha dos users
 User.create name: 'José', status: :active, kind: :salesman, email: 'sale@teste.com', password: '123123'
-User.create name: 'José2', status: :active, kind: :salesman, email: 'sale2@teste.com', password: '123123'
+User.create name: 'Manuel', status: :active, kind: :salesman, email: 'sale2@teste.com', password: '123123'
 User.create name: 'Marcos', status: :active, kind: :manager, email: 'manager@teste.com', password: '123123'
 
 # Criando alguns produtos de exemplo
-Product.create name: 'Smartphone', description:'Um smartphone novo ...', status: :active
-Product.create name: 'Tablet', description:'Um tablet novo ...', status: :active
+Product.create name: 'Smartphone', description:'Um smartphone novo ...', status: :active, price: 10
+Product.create name: 'Tablet', description:'Um tablet novo ...', status: :active, price: 20
 
 # Criando um desconto de exemplo
 Discount.create name: 'Desconto carnaval', description: 'Aplique esse desconto no carnaval', value: '10', kind: :porcent, status: :active
+Discount.create name: 'Desconto carnaval dinheiro', description: 'Aplique esse desconto quando possível', value: '10', kind: :money, status: :active
 
-
-puts 'seed done'
+# Crindo client
+Client.create name: 'Paulo', company_name: 'Google', document: '1234', email: 'paulo@google.com', user: User.first
+Client.create name: 'Julia', company_name: 'Google', document: 'abcd', email: 'julia@google.com', user: User.first
